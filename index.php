e93b0c356a8bd80a1008295e9052458075358494
Walerasan <walerasan@wp.pl>
2022-11-10 19:45:07 +0100

    Aktualizacja galezi development

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
  * Strona główna ProjektyDlaSzkol.pl copyright by: walerasan@gmail.com
  */
 ?>
-<?php echo("TEST APP");//get_header(); ?>
-<?php //get_template_part('content') ?> 		
-<?php //get_footer();?>
+<?php get_header(); ?>
+<?php get_template_part('content') ?> 		
+<?php get_footer();?>
